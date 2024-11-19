f397bf0e77fa7c6c796febe606c3b1a38d01cfb3
Kiva Vlad <melnilovv717@gmail.com>
2024-11-19 15:17:42 +0300

    fix graph

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,37 +1,52 @@
 import "./Chart.css";
 import { useEffect, useRef } from 'react';
-import { createChart } from 'lightweight-charts';
+import { createChart, IChartApi, ISeriesApi } from 'lightweight-charts';
 import { useStore } from "../../hooks/useStore";
 import { useTokenInfo } from "../../hooks/useTokenInfo";
 
 const Chart = () => {
   const { state } = useStore();
-  const { data } = useTokenInfo({symbol: state.symbol, interval: state.interval});
+  const { data } = useTokenInfo({ symbol: state.symbol, interval: state.interval });
   const chartContainerRef = useRef<HTMLDivElement | null>(null);
+  const chartRef = useRef<IChartApi | null>(null);
+  const candlestickSeriesRef = useRef<ISeriesApi<'Candlestick'> | null>(null);
 
   useEffect(() => {
     if (chartContainerRef.current) {
-      const chart = createChart(chartContainerRef.current, {
-        layout: { 
-          textColor: 'black', 
-          background: { 
-            color: 'white' 
-          } 
+      chartRef.current = createChart(chartContainerRef.current, {
+        layout: {
+          textColor: 'black',
+          background: {
+            color: 'white'
+          }
         }
       })
-      const candlestickSeries = chart.addCandlestickSeries({ upColor: '#26a69a', downColor: '#ef5350', borderVisible: false, wickUpColor: '#26a69a', wickDownColor: '#ef5350' });
-      candlestickSeries.setData(data);
-      chart.timeScale().fitContent();
 
-      return () => {
-        chart.remove();
+      candlestickSeriesRef.current = chartRef.current.addCandlestickSeries({
+        upColor: '#26a69a',
+        downColor: '#ef5350',
+        borderVisible: false,
+        wickUpColor: '#26a69a',
+        wickDownColor: '#ef5350'
+      })
+    }
+
+    return () => {
+      if (chartRef.current) {
+        chartRef.current.remove();
       }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (candlestickSeriesRef.current) {
+      candlestickSeriesRef.current.setData(data);
     }
   }, [data])
 
   return (
     <div className="chart">
-      <div ref={chartContainerRef} className="graph"/>
+      <div ref={chartContainerRef} className="graph" />
     </div>
   )
 }
